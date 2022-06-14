7f4b46e5d7a888b798f1e035c0a3b386088f1ce3
denvoiten <den.voiten@gmail.com>
2022-06-14 14:03:39 +0300

    UserController fix

--- src/main/java/ru/job4j/control/UserController.java
+++ src/main/java/ru/job4j/control/UserController.java
@@ -28,25 +28,15 @@ public class UserController {
         return "registration";
     }
 
-    @GetMapping("/fail")
-    public String failForm(Model model) {
-        model.addAttribute("message", "Ошибка регистрации!");
-        return "fail";
-    }
-
-    @GetMapping("/success")
-    public String successForm(Model model) {
-        model.addAttribute("message", "Регистрация прошла успешно!");
-        return "success";
-    }
-
     @PostMapping("/registration")
     public String registration(Model model, @ModelAttribute User user) {
         Optional<User> regUser = userService.add(user);
         if (regUser.isEmpty()) {
-            return "redirect:/fail";
+            model.addAttribute("message", "Ошибка регистрации!");
+            return "fail";
         }
-        return "redirect:/success";
+        model.addAttribute("message", "Регистрация прошла успешно!");
+        return "success";
     }
 
     @GetMapping("/loginPage")
